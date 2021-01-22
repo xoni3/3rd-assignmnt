e3740511a3e17b1566615b38f0c04a5877c1e343
xoni3 <johnnyifti@gmail.com>
2021-01-22 23:17:00 +0600

    java repo

--- assignment.js
+++ assignment.js
@@ -1,7 +1,6 @@
 // https://github.com/xoni3/3rd-assignmnt
 
 
-problem 1:
 
 function kilometerToMeter(kilometer){
     var meter = kilometer*1000;
@@ -12,33 +11,30 @@ var kilometer = kilometerToMeter(5);
 
 
 
-problem 2: 
+
 
 function budgetCalculator( watch, phone, laptop){
     var result = (watch + phone + laptop);
     return result
 }
-
 var total = budgetCalculator((50*2), (100*3), 500);
 
 
 
-problem 3:
+
+
 
 function hotelCost(days){
     var hotelCost = 0;
-
 if (days <=10 ) {
     hotelCost = days * 100;
 } 
-
 else if (days <=20 ) {
     var firstTenDays = 10 * 100;
     var remaining = days - 10;
     var secondTenDays = remaining * 80;
     hotelCost = firstTenDays + secondTenDays;
 }
-
 else{
     var firstTenDays = 10 * 100;
     var secondTenDays = 10 * 80;
@@ -52,7 +48,8 @@ var result = hotelCost(10);
 
 
 
-problem 4:
+
+
 
 function megaFreind(){
     var freinds = arguments[0];
